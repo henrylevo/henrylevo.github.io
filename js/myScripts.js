9bc009371168c63a9d6a9b81499a368ee7724f28
henrylevo-desktop <32999999+yoonlevo@users.noreply.github.com>
2017-11-17 17:02:18 +0100

    Starting up

---
+++ js/myScripts.js
@@ -0,0 +1,18 @@
+/*
+ * The JavaScript of Henry Levo's website
+ * Copyright, Henry Levo
+ * 17.11.2017
+ * 
+ */
+
+/* Table of contents
+––––––––––––––––––––––––––––––––––––––––––––––––––
+Main Page
+    - Menu
+    - Profile Picture
+
+Where Page
+    - Google Maps
+
+*/
+
