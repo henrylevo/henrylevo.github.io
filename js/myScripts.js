f9cc534f445f7ed892f17cbc9e3b1df6b56a7533
henrylevo-desktop <33756616+henrylevo@users.noreply.github.com>
2017-11-21 18:55:43 +0100

    add all on one page

--- js/myScripts.js
+++ js/myScripts.js
@@ -7,13 +7,131 @@
 
 /* Table of contents
 ––––––––––––––––––––––––––––––––––––––––––––––––––
-Main Page
-    - Menu
-    - Profile Picture
-
-Where Page
+Shared
+    - Meny nav
+    - Style Button
     - Google Maps
 
 */
 
+/* Meny nav
+–––––––––––––––––––––––––––––––––––––––––––––––––– */
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+    function openNav() {
+        document.getElementById("mySidenav").style.width = "300px";
+        document.getElementById("main").style.marginLeft = "300px";
+    }
 
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+    function closeNav() {
+        document.getElementById("mySidenav").style.width = "0";
+        document.getElementById("main").style.marginLeft = "0";
+    }
+
+
+/* Style Button
+–––––––––––––––––––––––––––––––––––––––––––––––––– */
+    function switchStyled() {
+        document.getElementById("classic-btn").id = "styled-btn";
+        document.getElementById("styled-btn").id = "classic-btn";
+    }
+
+    function switchClassic() {
+        document.getElementById("styled-btn").id = "classic-btn";
+        document.getElementById("classic-btn").id = "styled-btn";
+    }
+
+/* Google Maps
+–––––––––––––––––––––––––––––––––––––––––––––––––– */
+    function initMap() {
+        var posNow = {lat: 59.911491, lng: 10.757933};
+        var map = new google.maps.Map(document.getElementById('map'), 
+        {
+            zoom: 2,
+            center: posNow,
+            styles: [
+            {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
+            {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
+            {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
+            {
+                featureType: 'administrative.locality',
+                elementType: 'labels.text.fill',
+                stylers: [{color: '#d59563'}]
+            },
+            {
+                featureType: 'poi',
+                elementType: 'labels.text.fill',
+                stylers: [{color: '#d59563'}]
+            },
+            {
+                featureType: 'poi.park',
+                elementType: 'geometry',
+                stylers: [{color: '#263c3f'}]
+            },
+            {
+                featureType: 'poi.park',
+                elementType: 'labels.text.fill',
+                stylers: [{color: '#6b9a76'}]
+            },
+            {
+                featureType: 'road',
+                elementType: 'geometry',
+                stylers: [{color: '#38414e'}]
+            },
+            {
+                featureType: 'road',
+                elementType: 'geometry.stroke',
+                stylers: [{color: '#212a37'}]
+            },
+            {
+                featureType: 'road',
+                elementType: 'labels.text.fill',
+                stylers: [{color: '#9ca5b3'}]
+            },
+            {
+                featureType: 'road.highway',
+                elementType: 'geometry',
+                stylers: [{color: '#746855'}]
+            },
+            {
+                featureType: 'road.highway',
+                elementType: 'geometry.stroke',
+                stylers: [{color: '#1f2835'}]
+            },
+            {
+                featureType: 'road.highway',
+                elementType: 'labels.text.fill',
+                stylers: [{color: '#f3d19c'}]
+            },
+            {
+                featureType: 'transit',
+                elementType: 'geometry',
+                stylers: [{color: '#2f3948'}]
+            },
+            {
+                featureType: 'transit.station',
+                elementType: 'labels.text.fill',
+                stylers: [{color: '#d59563'}]
+            },
+            {
+                featureType: 'water',
+                elementType: 'geometry',
+                stylers: [{color: '#17263c'}]
+            },
+            {
+                featureType: 'water',
+                elementType: 'labels.text.fill',
+                stylers: [{color: '#515c6d'}]
+            },
+            {
+                featureType: 'water',
+                elementType: 'labels.text.stroke',
+                stylers: [{color: '#17263c'}]
+            }
+        ]
+        });
+        var marker = new google.maps.Marker({
+            position: posNow,
+            map: map
+        });
+    }
