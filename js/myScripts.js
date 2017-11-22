942919b3b3a6411983876715a97e4274ac90d03c
henrylevo-desktop <33756616+henrylevo@users.noreply.github.com>
2017-11-22 16:16:28 +0100

    update

--- js/myScripts.js
+++ js/myScripts.js
@@ -8,24 +8,24 @@
 /* Table of contents
 ––––––––––––––––––––––––––––––––––––––––––––––––––
 Shared
-    - Meny nav
+    - Menu nav
     - Style Button
     - Google Maps
 
 */
 
-/* Meny nav
+/* Menu nav
 –––––––––––––––––––––––––––––––––––––––––––––––––– */
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
     function openNav() {
-        document.getElementById("mySidenav").style.width = "300px";
-        document.getElementById("main").style.marginLeft = "300px";
+        document.getElementById("mySidenav").style.width = "100%";
+        //document.getElementById("main").style.marginRight = "300px";
     }
 
 /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
     function closeNav() {
         document.getElementById("mySidenav").style.width = "0";
-        document.getElementById("main").style.marginLeft = "0";
+        //document.getElementById("main").style.marginRight = "0";
     }
 
 
